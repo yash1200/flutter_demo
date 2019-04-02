05e217fc8833ebc17841b96ae90ee60f1ea4ceb6
Yash Johri <yashjohri1200@gmail.com>
2019-04-02 10:55:37 +0530

    mm

--- lib/DLinfo.dart
+++ lib/DLinfo.dart
@@ -1,6 +1,57 @@
 import 'package:flutter/material.dart';
 
 class DLinfo extends StatelessWidget {
+
+  var title = {
+  "Temporary Registration",
+  "Permanent Registration",
+  "Renewal Of Registrattions",
+  "Duplicate RC",
+  "No Objectionable Certificate",
+  "HP Indorcement",
+  "HP Termination",
+  "Address Change",
+  "Reassignment Of Vehicle",
+  "Trade Certificate",
+  "Certificate Issues",
+  "Ownership Transfer",
+  "Diplomatic Vehicles",
+  "Registartion Display"
+  };
+
+  buildList(BuildContext context) {
+    return ListView.builder(
+        itemCount: title.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, pos) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Container(
+              width: 100,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20, bottom: 20),
+                    child: Icon(
+                      Icons.account_balance,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    title.elementAt(pos),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 13,
+                        fontFamily: 'Muli',
+                        fontWeight: FontWeight.w400),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -22,185 +73,7 @@ class DLinfo extends StatelessWidget {
             padding: const EdgeInsets.only(right: 8.0),
             child: Container(
               height: 120,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(top: 20, bottom: 20),
-                          child: Icon(
-                            Icons.account_balance,
-                            color: Colors.blue,
-                          ),
-                        ),
-                        Center(
-                            child: Text(
-                          "Learner's DL",
-                              textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 13,
-                              fontFamily: 'Muli',
-                              fontWeight: FontWeight.w400),
-                        ))
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Container(
-                      width: 100,
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20, bottom: 20),
-                            child: Icon(
-                              Icons.account_balance,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          Text(
-                            "Permanent DL",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 13,
-                                fontFamily: 'Muli',
-                                fontWeight: FontWeight.w400),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Container(
-                      width: 100,
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20, bottom: 20),
-                            child: Icon(
-                              Icons.account_balance,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          Text(
-                            "Renewal Of DL",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 13,
-                                fontFamily: 'Muli',
-                                fontWeight: FontWeight.w400),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Container(
-                      width: 100,
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20, bottom: 20),
-                            child: Icon(
-                              Icons.account_balance,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          Text(
-                            "Duplicate DL",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 13,
-                                fontFamily: 'Muli',
-                                fontWeight: FontWeight.w400),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Container(
-                      width: 100,
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20, bottom: 20),
-                            child: Icon(
-                              Icons.account_balance,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          Text(
-                            "Addition Of Class",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 13,
-                                fontFamily: 'Muli',
-                                fontWeight: FontWeight.w400),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Container(
-                      width: 100,
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20, bottom: 20),
-                            child: Icon(
-                              Icons.account_balance,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          Text(
-                            "International Driving Permit",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 13,
-                                fontFamily: 'Muli',
-                                fontWeight: FontWeight.w400),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Container(
-                      width: 100,
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20, bottom: 20),
-                            child: Icon(
-                              Icons.account_balance,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          Text(
-                            "License Related Fees",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 13,
-                                fontFamily: 'Muli',
-                                fontWeight: FontWeight.w400),
-                          )
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
+              child: buildList(context),
             ),
           )
         ],
